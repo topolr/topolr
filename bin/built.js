77308c6209628e93b195eef43fa18910dc3a54d6
Jinliang <hou80houzhu@vip.qq.com>
2017-03-02 12:08:04 +0800

    uglify min file

---
+++ bin/built.js
@@ -0,0 +1,29 @@
+var topolr=require("topolr-util");
+var uglify=require("uglify-js");
+try {
+    var source=topolr.file(process.cwd() + "/app/topolr.js").readSync();
+    var a=source.match(/\/\*[\w\W]*?\*\//),comments=null;
+    if(a){
+        var comment=a[0];
+        var r=[];
+        comment.split(/\r|\n/).forEach(function (b) {
+            if(b){
+                r.push(b);
+            }
+        });
+        comments=r;
+    }
+    var result=uglify.minify(source, {
+        fromString: true,
+        mangle: true
+    }).code;
+    var hash=topolr.hash.md5(result);
+    if(comments){
+        comments.splice(comments.length-1,0," * md5 "+hash);
+    }
+    topolr.file(process.cwd() + "/dist/topolr.min.js").write((comments?comments.join("\r\n")+"\r\n":"")+result).done(function () {
+        topolr.log("(color:green=> BUILT DONE,MD5:[{{hash}}])",{hash:hash});
+    });
+}catch(e){
+    console.log(e.stack);
+}
