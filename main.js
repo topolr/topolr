6fc8842e544cc82e9b571859d3cc1a674da03a99
topolr <topolr@vip.qq.com>
2017-03-09 15:58:41 +0800

    npm

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var topolr=require("topolr-util");
 module.exports={
     getSourceCode:function () {
-        return topolr.file("./src/topolr.js").readSync();
+        return topolr.file(__dirname+"/src/topolr.js").readSync();
     },
     getMiniCode:function () {
-        return topolr.file("./dist/topolr.min.js").readSync();
+        return topolr.file(__dirname+"/dist/topolr.min.js").readSync();
     },
     copySourceTo:function (path) {
-        return topolr.file("./src/topolr.js").copyTo(path);
+        return topolr.file(__dirname+"/src/topolr.js").copyTo(path);
     },
     copyMiniTo:function (path) {
-        return topolr.file("./dist/topolr.mini.js");
+        return topolr.file(__dirname+"/dist/topolr.mini.js");
     }
 };
